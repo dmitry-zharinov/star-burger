d241a50269463069281c2716b4460200ad5d2191
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-11-01 21:56:23 +0300

    Alter field updated_at on location

--- geolocation/models.py
+++ geolocation/models.py
@@ -5,7 +5,7 @@ class Location(models.Model):
     address = models.CharField('адрес', max_length=100, unique=True)
     lat = models.FloatField('широта', null=True, blank=True)
     lon = models.FloatField('долгота', null=True, blank=True)
-    updated_at = models.DateTimeField('дата запроса к геокодеру')
+    updated_at = models.DateTimeField('дата запроса к геокодеру', auto_now=True)
 
     def __str__(self):
         return f'{self.address} ({self.lat}, {self.lon})'
