0ae1b52d9a62cd1463adb490955a1d3b08cd5d60
dmitry-zharinov <zharinovdm@yandex.ru>
2022-12-30 10:55:27 +0000

    Fix deploy script errors

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -12,7 +12,6 @@ git pull
 echo -e "\n${BOLD}Установка библиотек для Python...${BOLD_END}"
 source venv/bin/activate
 pip install -r requirements.txt
-deactivate
 
 echo -e "\n${BOLD}Установка библиотек для Node.js...${BOLD_END}"
 npm install --only=dev
@@ -21,11 +20,13 @@ echo -e "\n${BOLD}Сборка фронтенда...${BOLD_END}"
 parcel build bundles-src/index.js -d bundles --no-minify --public-url="./"
 
 echo -e "\n${BOLD}Сборка статики Django...${BOLD_END}"
-yes yes | venv/bin/python manage.py collectstatic
+yes yes | venv/bin/python manage.py collectstatic --no-input
 
 echo -e "\n${BOLD}Применение миграций...${BOLD_END}"
-venv/bin/python manage.py migrate
+venv/bin/python manage.py migrate --no-input
 
 echo -e "\n${BOLD}Перезапуск сервисов Systemd...${BOLD_END}"
 systemctl restart starburger.service
 systemctl reload nginx.service
+
+echo -e "\n${BOLD}Деплой проекта успешно завершён!${BOLD_END}"
